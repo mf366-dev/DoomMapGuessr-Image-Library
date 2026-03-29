a4ff2a94f778d38998164dbddf594d226f1200f7
Matthew <real_mf366@yahoo.com>
2026-03-29 22:21:38 +0100

    We are not going to be using ASCIIMATICS
    I think we'll go for a good old homemade TUI

--- maintenance/main.py
+++ maintenance/main.py
@@ -48,10 +48,6 @@ import re
 import csv
 import sqlite3
 import requests
-from asciimatics.widgets import Frame, ListBox, Layout, Divider, Text, Button, TextBox, Widget
-from asciimatics.scene import Scene
-from asciimatics.screen import Screen
-from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
 
 
 # [*] Constants
